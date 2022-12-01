183ade7afed2ae900be0e8f4fb6e3458144871f3
kinduff <kinduff@protonmail.com>
2022-12-01 10:42:23 +0100

    Day 1

---
+++ solutions/day01.rb
@@ -0,0 +1,43 @@
+# frozen_string_literal: true
+
+require_relative '../lib/utils'
+
+input = IO.read('day01.txt', chomp: true)
+
+test_input = '1000
+2000
+3000
+
+4000
+
+5000
+6000
+
+7000
+8000
+9000
+
+10000
+'
+
+# --- Part One ---
+
+def solve_1(input)
+  input.split("\n\n").map do |nums|
+    nums.split("\n").map(&:to_i).inject(:+)
+  end.max
+end
+
+assert_equal solve_1(test_input), 24000
+psol '1', solve_1(input)
+
+# --- Part Two ---
+
+def solve_2(input)
+  input.split("\n\n").map do |nums|
+    nums.split("\n").map(&:to_i).inject(:+)
+  end.sort.reverse.first(3).inject(:+)
+end
+
+assert_equal solve_2(test_input), 45000
+psol '2', solve_2(input)
